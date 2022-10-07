2b763a64b97a0d75b28021c818fc27720dc52aff
MatheusMax01 <samuel.tomimatu@bandtec.com.br>
2022-10-07 13:03:05 -0300

    add cpf e datanascimento

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -4,6 +4,7 @@ function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var cpf = sessionStorage.CPF_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
 
--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -65,6 +65,8 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var cpf = req.body.cpfServer;
+    var nasc = req.body.nascServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -73,10 +75,14 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else {
+    }else if (cpf == undefined) {
+        res.status(400).send("Seu cpf está undefined!");
+    }else if (nasc == undefined) {
+        res.status(400).send("Sua data de nascimento está undefined!");
+    }else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, cpf, nasc)
             .then(
                 function (resultado) {
                     res.json(resultado);
